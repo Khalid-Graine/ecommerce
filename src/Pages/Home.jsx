2c30ec55ca8597d19119c4e6206de5c0015db8c5
Khalid Graine <khalidgraine1@gmail.com>
2024-02-23 23:40:38 +0100

    edit icons

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { IconTruckFast, TruckFast } from "../assets/icons/MyIcons";
 import CenterArea from "../components/parts/home/CenterArea";
 import LeftSideBar from "../components/parts/home/LeftSideBar";
 import RightSideBar from "../components/parts/home/RightSideBar";
@@ -7,14 +8,23 @@ const Home = () => {
     <>
       <div className="flex">
         {/* left side */}
-         <LeftSideBar />
-          {/* center */}
-         <CenterArea />
-          {/* right */}
-          <RightSideBar />
+        <LeftSideBar />
+        {/* center */}
+        <CenterArea />
+        {/* right */}
+        <RightSideBar />
       </div>
-      <div className="flex justify-center gap-2 py-2 bg-gray-400">
-        <span>X</span>
+      <div className="flex justify-center gap-2 bg-gray-400 py-2">
+        {/* <img src="src/assets/icons/truck-fast.svg" className=" text-red-700" alt="" /> */}
+
+        <div className="flex items-center justify-center">
+          {/* <IconTruckFast className=" text-4xl text-gray-800 hover:text-red-700" />
+       <IconTruckFast /> */}
+          {/* <IconTruckFast /> */}
+          <IconTruckFast className="bg-yellow-400 text-3xl" />
+          
+        </div>
+        <TruckFast className="bg-yellow-400 text-3xl  " />
         <p>FREE FAST SHIPPING ON ORDERS ABOVE $100</p>
       </div>
       
