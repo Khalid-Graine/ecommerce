8fecf9034773b785ba8fd6be0b845aab18e4040c
Khalid Graine <khalidgraine1@gmail.com>
2024-02-23 20:30:09 +0100

    built SIGN UP AND SAVE part

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
       <div className="flex  ">
           <div className="w-[260px] bg-blue-100" >left side</div>
           <div className=" flex-grow  bg-red-100" >centent</div>
-          <div className="w-[260px] bg-yellow-300 py-2 px-4">
+          <div className="w-[260px] bg-yellow-300 py-2 px-4 flex flex-col ">
             <div>
               <h4 className="title">top categories</h4>
               
@@ -21,8 +21,20 @@ const Home = () => {
                 </ul>
               
             </div>
-            <div></div>
-            <div></div>
+            <div className="line"></div>
+            <div>
+            <h4 className="title">SIGN UP AND SAVE</h4>
+            <p>Receive a $10 voucher in your inbox immediately after you sign up for our newsletter.</p>
+            <form action="">
+              <input type="text" />
+              <button>sign</button>
+              <input type="checkbox" name="" id="" />
+            </form>
+            </div>
+            <div className="line"></div>
+            <div>
+            <h4 className="title">GALLERY MODULE</h4>
+            </div>
           </div>
       </div>
       
