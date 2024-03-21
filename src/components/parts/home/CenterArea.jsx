694187fa24807d83771bef9746c9f21812498a51
Khalid Graine <khalidgraine1@gmail.com>
2024-03-21 22:28:13 +0000

    design modules

--- src/components/parts/home/CenterArea.jsx
+++ src/components/parts/home/CenterArea.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
 const CenterArea = () => {
+  const modules = [
+    {
+      title: "Lorem ipsum dolor sit.",
+      icon: "fa-solid fa-fire-flame-curved text-2xl text-red-600",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eum.",
+    },
+    {
+      title: "Lorem ipsum dolor sit.",
+      icon: "fa-solid fa-fire-flame-curved text-2xl text-red-600",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eum.",
+    },
+    {
+      title: "Lorem ipsum dolor sit.",
+      icon: "fa-solid fa-fire-flame-curved text-2xl text-red-600",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, eum.",
+    },
+  ];
   return (
     <div className="flex-grow border  border-y-0 border-black bg-red-100 p-4">
-      <div className="grid grid-cols-6 gap-4 bg-purple-500 ">
+      {/* first div */}
+      <div className="grid grid-cols-6 gap-4 bg-purple-500 pb-4">
         <div className=" col-span-6 h-[300px] w-full bg-green-500"></div>
-        <div className="relative col-span-3 flex h-[100px] w-full items-center justify-center overflow-hidden bg-blue-400">
+        <div className="relative col-span-full flex h-[150px] w-full items-center justify-center overflow-hidden bg-blue-400 sm:col-span-3">
           <img
             className=" object-contain object-center "
             src="src/assets/iphone.jpg"
@@ -15,49 +36,31 @@ const CenterArea = () => {
             test banner 1
           </button>
         </div>
-        <div className="relative col-span-3 flex h-[100px] w-full items-center justify-center overflow-hidden bg-blue-400">
+        <div className="relative col-span-full flex h-[150px] w-full items-center justify-center overflow-hidden bg-blue-400 sm:col-span-3">
           <img
             className=" object-contain object-center "
-            src="src/assets/iphone.jpg"
+            src="src/assets/iphone2.webp"
             alt=""
           />
           <button className="absolute bg-green-500 px-2 py-1 font-semibold uppercase text-white hover:bg-green-600 active:bg-green-500">
             test banner 2
           </button>
         </div>
-
-        <div></div>
       </div>
+
+      {/* second div */}
       <div className="grid grid-cols-1 gap-1 bg-red-700 lg:grid-cols-12">
-        <div className=" col-span-4 flex bg-yellow-800">
-          <div className="flex items-center justify-center px-4">icon</div>
-          <div>
-            <h3 className=" font-bold">Lorem ipsum dolor sit.</h3>
-            <p>Create and open custom popups from any link or block</p>
+        {modules.map((module, index) => (
+          <div key={index} className=" col-span-4 flex bg-slate-800 py-2">
+            <div className="flex items-center justify-center px-4">
+              <i className={module.icon}></i>
+            </div>
+            <div>
+              <h3 className="font-bold text-white">{module.title}</h3>
+              <p className=" text-gray-600">{module.description}</p>
+            </div>
           </div>
-        </div>
-        <div className=" col-span-4 flex bg-yellow-800">
-          <div className="flex items-center justify-center px-4">icon</div>
-          <div>
-            <h3 className=" font-bold">Lorem ipsum dolor sit.</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              quas et deserunt impedit aliquam culpa fuga corrupti autem saepe
-              a?
-            </p>
-          </div>
-        </div>
-        <div className=" col-span-4 flex bg-yellow-800">
-          <div className="flex items-center justify-center px-4">icon</div>
-          <div>
-            <h3 className=" font-bold">Lorem ipsum dolor sit.</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              quas et deserunt impedit aliquam culpa fuga corrupti autem saepe
-              a?
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
