86a0897896d98b54afc0d2912f5848b0cdd74ee2
Khalid Graine <khalidgraine1@gmail.com>
2024-04-29 11:16:54 +0100

    design the notification module

--- src/components/NotificationModule.jsx
+++ src/components/NotificationModule.jsx
@@ -9,13 +9,17 @@ const NotificationModule = () => {
   return (
   
          <div
-          
-          className={`Notification-Module fixed w-full bg-red-300 p-3 transition-all ease-in-out  duration-1000
+          className={`Notification-Module fixed w-full bg-black text-white bg-opacity-80 px-4 py-3 transition-all ease-in-out  duration-1000
           ${showNotification ? "bottom-0" : "-bottom-full"}`}
         >
-          <button onClick={handleNotificationClose}>X</button>
-          <p>Notification Module</p>
-          <p>
+           <div className="flex justify-between">
+           <h3 className=" font-bold text-lg">Notification Module</h3>
+          <button onClick={handleNotificationClose} className="bg-red-600 size-8">
+          <i className="fa-solid fa-xmark"></i>
+          </button>
+           </div>
+          
+          <p className="w-11/12">
             This is the sticky Notification module. You can use it for any
             sticky messages such as cookie notices or special promotions, etc.
           </p>
