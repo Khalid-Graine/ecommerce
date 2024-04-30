c341879038e31bc40306bb7a667378ab655f40e2
Khalid Graine <khalidgraine1@gmail.com>
2024-04-30 12:21:24 +0100

    finish the structure

--- src/components/parts/home/FeaturesSection.jsx
+++ src/components/parts/home/FeaturesSection.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const FeaturesSection = () => {
   return (
-    <div className="sides bg-slate-300 py-6">
+    <div className="sides bg-slate-500 py-6">
       <h3 className="py-6 text-center font-bold text-2xl">Why Buy From Us?</h3>
-      <ul className="grid grid-cols-2 gap-6">
+      <ul className="grid grid-cols-2 lg:grid-cols-4 gap-y-8 gap-x-3">
         <FeatureItem
           iconClass="fa-hourglass-start"
           title="Title 1"
--- src/layout/TheFooter.jsx
+++ src/layout/TheFooter.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 const TheFooter = () => {
-  return <div className="sides h-28 bg-gray-800">TheFooter</div>;
+  return (
+    <div className="">
+      <div>links</div>
+      <div className="sides grid grid-cols-1 bg-[#303841] lg:grid-cols-2">
+        <div>
+        <h3 className="text-lg font-semibold text-white uppercase">follow us</h3>
+          <div className="my-2"></div>
+          <SocialMedia />
+          <p>Copyright © 2019, Your Store, All Rights Reserved</p>
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold text-white uppercase">NEWSLETTER</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TheFooter;
+
+const SocialMedia = () => {
+  return (
+    <ul className="flex gap-3 *:flex *:size-11 *:cursor-pointer *:items-center *:justify-center *:rounded-full *:bg-blue-500 *:text-2xl *:text-white">
+      <li>
+        <i className="fa-brands fa-facebook-f"></i>
+      </li>
+      <li>
+        <i className="fa-brands fa-x-twitter"></i>
+      </li>
+      <li>
+        <i className="fa-brands fa-pinterest-p"></i>
+      </li>
+      <li>
+        <i className="fa-brands fa-instagram"></i>
+      </li>
+      <li>
+        <i className="fa-brands fa-whatsapp"></i>
+      </li>
+      <li>
+        <i className="fa-brands fa-reddit-alien"></i>
+      </li>
+    </ul>
+  );
+};
