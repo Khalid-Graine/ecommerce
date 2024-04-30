ed19c9056b8ab80ffae29bf965a0050e19c322cc
Khalid Graine <khalidgraine1@gmail.com>
2024-04-30 11:44:55 +0100

    building Why Buy From Us section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,9 +13,42 @@ const Home = () => {
         {/* right */}
         <RightSideBar />
       </div>
-      <div className="flex justify-center items-center gap-1 py-2 bg-gray-400">
+      <div className="flex items-center justify-center gap-1 bg-[#d3d3d3] py-3  text-[#333333] ">
         <i className="fa-solid fa-truck-fast"></i>
         <p>FREE FAST SHIPPING ON ORDERS ABOVE $100</p>
+      </div>
+      <div className="sides">
+        <h3 className="my-3 mt-5 text-center">Why Buy From Us?</h3>
+        <ul className="bg-green-100 grid grid-cols-2 gap-6">
+          <li className="flex flex-col items-center gap-2">
+            <div className="bg-red-300 size-20 flex justify-center items-center rounded-full *:text-3xl">
+            <i className="fa-solid fa-hourglass-start"></i>
+            </div>
+            <h3>title</h3>
+            <p>description Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li className="flex flex-col items-center gap-2">
+            <div className="bg-red-300 size-20 flex justify-center items-center rounded-full *:text-3xl">
+            <i className="fa-solid fa-hourglass-start"></i>
+            </div>
+            <h3>title</h3>
+            <p>description Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li className="flex flex-col items-center gap-2">
+            <div className="bg-red-300 size-20 flex justify-center items-center rounded-full *:text-3xl">
+            <i className="fa-solid fa-hourglass-start"></i>
+            </div>
+            <h3>title</h3>
+            <p>description Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li className="flex flex-col items-center gap-2">
+            <div className="bg-red-300 size-20 flex justify-center items-center rounded-full *:text-3xl">
+            <i className="fa-solid fa-hourglass-start"></i>
+            </div>
+            <h3>title</h3>
+            <p>description Lorem ipsum dolor sit amet.</p>
+          </li>
+        </ul>
       </div>
     </>
   );
