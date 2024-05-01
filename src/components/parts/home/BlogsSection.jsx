193c94fe2f5a6c9680dbe834cef47867ffc7ec4b
Khalid Graine <khalidgraine1@gmail.com>
2024-05-01 13:49:34 +0100

    import react carousel

--- src/components/parts/home/BlogsSection.jsx
+++ src/components/parts/home/BlogsSection.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 const BlogsSection = () => {
   return (
     <div>
-        
+        <div>From the Blog</div>
     </div>
   )
 }
