5e5f8140e6af6bf1c37675ce011c470c188be087
Khalid Graine <khalidgraine1@gmail.com>
2024-05-02 12:03:26 +0100

    add Label Title ReadMoreBtn

--- src/components/parts/home/HomeCarousel.jsx
+++ src/components/parts/home/HomeCarousel.jsx
@@ -10,22 +10,22 @@ const HomeCarousel = () => {
       title: "tlorem taf thiiim e msms ",
       link: "#",
       imagePath: "src/assets/iphone.jpg",
-      label: "new",
-      butoonText: 'shop now'
+      label: "phone",
+      butoonText: "shop now",
     },
     {
       title: "tlorem taf thiiim e msms ",
       link: "#",
       imagePath: "src/assets/iphone.jpg",
       label: "new",
-      butoonText: 'shop now'
+      butoonText: "shop now",
     },
     {
       title: "tlorem taf thiiim e msms ",
       link: "#",
       imagePath: "src/assets/iphone.jpg",
       label: "new",
-      butoonText: 'shop now'
+      butoonText: "shop now",
     },
   ];
   return (
@@ -47,11 +47,9 @@ const CarouselItem = ({ item }) => {
   return (
     <div className=" relative flex h-[300px] items-center overflow-auto rounded-sm p-4 hover:cursor-pointer">
       <div className="flex flex-col gap-2">
-        <p className="w-fit rounded-sm  bg-blue-600 px-2  py-1 font-semibold text-white">
-          {item.label}
-        </p>
-        <h3 className=" text-3xl font-bold text-white">{item.title}</h3>
-        <MoreButton text={item.butoonText} link={item.link} />
+        <Label>{item.label}</Label>
+        <Title>{item.title}</Title>
+        <ReadMoreButton text={item.butoonText} link={item.link} />
       </div>
       <img
         src={item.imagePath}
@@ -61,14 +59,26 @@ const CarouselItem = ({ item }) => {
     </div>
   );
 };
-
-const MoreButton = ({ text, link }) => {
+const Title = ({ children }) => {
+  return (
+    <div className=" text-3xl font-bold capitalize text-white">{children}</div>
+  );
+};
+const ReadMoreButton = ({ text, link }) => {
   return (
     <a href={link}>
       <button className="flex  w-fit items-center gap-2 rounded-sm border px-3 py-1 text-white transition-all duration-100 ease-linear hover:gap-3">
-        <span>{text}</span>
+        <span className="capitalize">{text}</span>
         <i className="fa-solid fa-arrow-right-long"></i>
       </button>
     </a>
+  );
+};
+
+const Label = ({ children }) => {
+  return (
+    <p className="w-fit rounded-sm  bg-blue-600 px-2  py-1 font-semibold capitalize text-white">
+      {children}
+    </p>
   );
 };
