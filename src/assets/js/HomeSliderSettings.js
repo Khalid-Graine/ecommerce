6b30b13722d173513a10c04f43b681b0b54e2d0e
Khalid Graine <khalidgraine1@gmail.com>
2024-05-02 10:07:42 +0100

    built HomeSliderSettings file

--- src/assets/js/HomeSliderSettings.js
+++ src/assets/js/HomeSliderSettings.js
@@ -1,9 +1,8 @@
 // sliderSettings.js
 export const HomeSliderSettings = {
-    dots: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    initialSlide: 0,
-  };
-  
+  dots: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  initialSlide: 0,
+};
