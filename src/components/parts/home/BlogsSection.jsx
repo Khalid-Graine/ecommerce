a534bf363c788484b58e95fff6b94a1f08088e63
Khalid Graine <khalidgraine1@gmail.com>
2024-05-02 10:30:09 +0100

    customize the home carousel

--- src/components/parts/home/BlogsSection.jsx
+++ src/components/parts/home/BlogsSection.jsx
@@ -1,6 +1,6 @@
 import { sliderSettings } from "./../../../assets/js/sliderSettings";
 import { useState } from "react";
-import "../../../assets/css/BlogsCarousel.css";
+import "../../../assets/css/HomeCarousel.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
