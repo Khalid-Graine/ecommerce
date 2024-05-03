c517d81016f9dc3199054471fbb2cefe299ef60d
Khalid Graine <khalidgraine1@gmail.com>
2024-05-03 12:10:48 +0100

    map over TestimonialsData

--- src/components/parts/home/LeftSideBar.jsx
+++ src/components/parts/home/LeftSideBar.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -8,16 +6,26 @@ import "../../../assets/css/TestimonialsCarousel.css";
 import { TestimonialsSliderSettings } from "../../../assets/js/TestimonialsSliderSettings";
 
 const LeftSideBar = () => {
-  const Testimonials = [{
-    user: 'test',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At distinctio necessitatibus voluptates facilis non, ab repudiandae ipsam qui enim cupiditate id nisi eius dolor iusto saepe laboriosam eligendi nemo facere.'
-  }]
+  const TestimonialsData = [
+    {
+      user: "test",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At distinctio necessitatibus voluptates facilis non, ab repudiandae ipsam qui enim cupiditate id nisi eius dolor iusto saepe laboriosam eligendi nemo facere.",
+    },
+    {
+      user: "test",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At distinctio necessitatibus voluptates facilis non, ab repudiandae ipsam qui enim cupiditate id nisi eius dolor iusto saepe laboriosam eligendi nemo facere.",
+    },
+    {
+      user: "test",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At distinctio necessitatibus voluptates facilis non, ab repudiandae ipsam qui enim cupiditate id nisi eius dolor iusto saepe laboriosam eligendi nemo facere.",
+    },
+  ];
   return (
-    
-    <div className="hidden  
-     max-w-[230px] flex-col  px-4 py-2 lm:flex">
+    <div
+      className="hidden  
+     max-w-[230px] flex-col  px-4 py-2 lm:flex"
+    >
       <div>
-       
         <p className="title">title</p>
         <ul>
           <li>Product 1</li>
@@ -29,12 +37,17 @@ const LeftSideBar = () => {
       <div className="line"></div>
       <div>
         <p className="title">WHAT PEOPLE SAY:</p>
-        <TestimonialsCarousel />
-       <div className="Testimonials-Carousel max-w-min">
-       <Slider className="* *:bg-yellow-400 " {...TestimonialsSliderSettings}>
-          
-        </Slider>
-       </div>
+        
+        <div className="Testimonials-Carousel max-w-min">
+          <Slider
+            className="* *:bg-yellow-400 "
+            {...TestimonialsSliderSettings}
+          >
+            {TestimonialsData.map((item,index) => (
+              <CarouselItem key={index} item={item} /> 
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
@@ -42,9 +55,10 @@ const LeftSideBar = () => {
 
 export default LeftSideBar;
 
-
-const CarouselItem = () => {
-  return (
-    <div>e</div>
-  )
-}
+const CarouselItem = ({item}) => {
+  return <div>
+         
+         <p>{item.text}</p>
+         <p>{item.user}</p>
+  </div>;
+};
