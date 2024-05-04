a960b62919cb648e3da21df87ad83736df0e15a4
Khalid Graine <khalidgraine1@gmail.com>
2024-05-04 15:05:44 +0100

    adjust the gap

--- src/components/parts/home/BestSelection.jsx
+++ src/components/parts/home/BestSelection.jsx
@@ -101,7 +101,7 @@ const BestSelection = () => {
             <h3>Our Best Selection</h3>
             <p>Display products on any page from a variety of sources. Journal 3 offers the most comprehensive product selection tools in Opencart.</p>
         </div>
-        <div className='bg-yellow-300 grid grid-cols-2 lg:grid-cols-4 gap-2'>
+        <div className='mt-6 bg-yellow-300 grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-5'>
             {Selections.map((item,index)=> (
                 <div key={index}>
                      <TheTitle>{item.title} </TheTitle>
