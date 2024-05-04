e8d9f964c97f6f20ff1253b598ef7d947fc81f45
Khalid Graine <khalidgraine1@gmail.com>
2024-05-04 14:19:24 +0100

    structure the component

--- src/components/parts/home/BestSelection.jsx
+++ src/components/parts/home/BestSelection.jsx
@@ -7,11 +7,12 @@ const BestSelection = () => {
             <h3>Our Best Selection</h3>
             <p>Display products on any page from a variety of sources. Journal 3 offers the most comprehensive product selection tools in Opencart.</p>
         </div>
-        <div className='bg-yellow-300'>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className='bg-yellow-300 grid grid-cols-2 lg:grid-cols-4 gap-2'>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In optio amet sint quidem enim nisi quam necessitatibus odit aperiam ipsa.</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In optio amet sint quidem enim nisi quam necessitatibus odit aperiam ipsa.</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In optio amet sint quidem enim nisi quam necessitatibus odit aperiam ipsa.</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. In optio amet sint quidem enim nisi quam necessitatibus odit aperiam ipsa.</div>
+            
         </div>
     </div>
   )
